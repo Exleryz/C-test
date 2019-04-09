3fbab41a7f83f7c180979edf6acf0ce3a491c25b
Exleryz <zyezhou@163.com>
2019-04-09 13:32:22 +0800

    LeetCode 2. Add Two Numbers

--- python/LeetCode/2. Add Two Numbers.py
+++ python/LeetCode/2. Add Two Numbers.py
@@ -31,35 +31,88 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        l3 = None
-        list30 = None
-        temp0 = 0
-        while l1 is None or l2 is None or temp0 = 1:
-            if l1 is None:
-                temp1 = 0
-            else:
-                temp1 = l1.val
-                l1 = l1.next
-            if l2 is None:
-                temp2 = 0
-            else:
-                temp2 = l2.val
-                l2 = l2.next
+        # l3 = None
+        # list30 = None
+        # temp0 = 0
+        # while l1 is None or l2 is None or temp0 = 1:
+        #     if l1 is None:
+        #         temp1 = 0
+        #     else:
+        #         temp1 = l1.val
+        #         l1 = l1.next
+        #     if l2 is None:
+        #         temp2 = 0
+        #     else:
+        #         temp2 = l2.val
+        #         l2 = l2.next
 
-            temp = temp1 + temp2 + temp0
-            if temp >= 10:
-                temp0 = 1
-                temp -= 10
-            else:
-                temp0 = 0
-            list3 = ListNode(temp)
-            if l3 is None:
-                list30 = list3
-                l3 = list30
-            else:
-                list30.next = list3
-                list30 = list30.next
-        return l3
+        #     temp = temp1 + temp2 + temp0
+        #     if temp >= 10:
+        #         temp0 = 1
+        #         temp -= 10
+        #     else:
+        #         temp0 = 0
+        #     list3 = ListNode(temp)
+        #     if l3 is None:
+        #         list30 = list3
+        #         l3 = list30
+        #     else:
+        #         list30.next = list3
+        #         list30 = list30.next
+        # return l3
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        carry = 0
+        to_return = l1
+
+        while True:
+            ans = l1.val + l2.val + carry
+            carry = 0
+            if ans > 9:
+                carry = ans//10
+                ans = ans % 10
+            l1.val = ans
+            if l1.next == None or l2.next == None:
+                break
+            l1 = l1.next
+            l2 = l2.next
+
+        if l1.next == None and l2.next == None:
+            if carry > 0:
+                l1.next = ListNode(carry)
+                l1 = l1.next
+                carry = 0
+                return to_return
+
+        while l1.next != None:
+            l1 = l1.next
+            ans = l1.val + carry
+            carry = 0
+            if ans > 9:
+                carry = ans // 10
+                ans = ans % 10
+            l1.val = ans
+            print(l1.val)
+
+        while l2.next != None:
+            l2 = l2.next
+            ans = l2.val + carry
+            carry = 0
+            l1.next = ListNode(ans)
+            l1 = l1.next
+            if ans > 9:
+                carry = ans // 10
+                ans = ans % 10
+                l1.val = ans
+
+        if carry > 0:
+            l1.next = ListNode(carry)
+            l1 = l1.next
+            carry = 0
+
+        return to_return
 
 
 s = Solution()
